3753cf5900110b3379af391906f5c31e70d0f8b6
christina-gorina <christina.gorina@yandes.ru>
2022-02-03 22:11:06 +0300

    рефакторинг

--- src/main/java/com/christinagorina/endpoint/InputEndpoint.java
+++ src/main/java/com/christinagorina/endpoint/InputEndpoint.java
@@ -1,46 +1,19 @@
 package com.christinagorina.endpoint;
 
-import com.christinagorina.config.CountryRepository;
 import com.christinagorina.countries.Country;
 import com.christinagorina.countries.Currency;
 import com.christinagorina.countries.GetCountryRequest;
 import com.christinagorina.countries.GetCountryResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
-/*
-@Endpoint
-@RequiredArgsConstructor
-@Slf4j
-public class InputEndpoint {
-
-    @PayloadRoot(localPart = "MainInfo")
-    @ResponsePayload
-    @RequestMapping("/input")
-    public Answer inputInformation(@RequestPayload Element element) {
-        log.info("inputInformation");
-        log.info("inputInformation element = " + element);
-        Answer answer = new Answer();
-        answer.setAnswer("It is answer");
-        return answer;
-    }
-}
-*/
 @Endpoint
 @Slf4j
 public class InputEndpoint {
     private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
-
-    private CountryRepository countryRepository;
-
-    @Autowired
-    public InputEndpoint(CountryRepository countryRepository) {
-        this.countryRepository = countryRepository;
-    }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
     @ResponsePayload
