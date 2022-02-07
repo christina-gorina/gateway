b4bfc26969998dccc2a76eec44d4b4b4d4298e6c
christina-gorina <christina.gorina@yandes.ru>
2022-02-07 22:33:34 +0300

    inputInfoRequest и inputInfoResponse

--- src/main/java/com/christinagorina/config/WebServiceConfig.java
+++ src/main/java/com/christinagorina/config/WebServiceConfig.java
@@ -5,9 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurationSupport;
-import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -23,18 +21,18 @@ public class WebServiceConfig extends WsConfigurationSupport {
 		return new ServletRegistrationBean<>(servlet, "/ws/*");
 	}
 
-	@Bean(name = "countries")
-	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+	@Bean(name = "constructors")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema constructorsSchema) {
 		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
-		wsdl11Definition.setPortTypeName("CountriesPort");
+		wsdl11Definition.setPortTypeName("ConstructorsPort");
 		wsdl11Definition.setLocationUri("/ws");
-		wsdl11Definition.setTargetNamespace("http://spring.io/guides/gs-producing-web-service");
-		wsdl11Definition.setSchema(countriesSchema);
+		wsdl11Definition.setTargetNamespace("http://christina.gorina.com");
+		wsdl11Definition.setSchema(constructorsSchema);
 		return wsdl11Definition;
 	}
 
 	@Bean
-	public XsdSchema countriesSchema() {
-		return new SimpleXsdSchema(new ClassPathResource("schema/countries.xsd"));
+	public XsdSchema constructorsSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("schema/inputInfo.xsd"));
 	}
 }
--- src/main/java/com/christinagorina/endpoint/InputEndpoint.java
+++ src/main/java/com/christinagorina/endpoint/InputEndpoint.java
@@ -1,8 +1,8 @@
 package com.christinagorina.endpoint;
 
-import com.christinagorina.countries.Country;
-import com.christinagorina.countries.Currency;
-import com.christinagorina.countries.GetCountryResponse;
+import com.christina.gorina.xmlModels.Constructor;
+import com.christina.gorina.xmlModels.Currency;
+import com.christina.gorina.xmlModels.InputInfoResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -13,21 +13,21 @@ import org.w3c.dom.Element;
 @Endpoint
 @Slf4j
 public class InputEndpoint {
-    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service"; // может быть любой, главное что бы был указан в dto и настройках
+    private static final String NAMESPACE_URI = "http://christina.gorina.com";
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "inputInfoRequest")
     @ResponsePayload
-    public GetCountryResponse getCountry(@RequestPayload Element request) {
-        log.info("getCountry qwe request = " + request);
-        GetCountryResponse getCountryResponse = new GetCountryResponse();
-        Country country = new Country();
-        country.setName("FGFHJJGFJHGHGG");
-        country.setCapital("rerterterter");
-        country.setPopulation(4);
-        country.setCurrency(Currency.GBP);
-        getCountryResponse.setCountry(country);
-        log.info("getCountry qwe getCountryResponse = " + getCountryResponse);
-        return getCountryResponse;
+    public InputInfoResponse getConstructor(@RequestPayload Element request) {
+        log.info("getConstructor request = " + request);
+        InputInfoResponse inputInfoResponse = new InputInfoResponse();
+        Constructor constructor = new Constructor();
+        constructor.setName("Stiv");
+        constructor.setIncome(1000000);
+        constructor.setCurrency(Currency.GBP);
+        constructor.setIsConstant(true);
+        inputInfoResponse.setConstructor(constructor);
+        log.info("inputInfoResponse = " + inputInfoResponse);
+        return inputInfoResponse;
     }
 }
 
